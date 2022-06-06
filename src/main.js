82734600aa290d564b2cb89634bb6bc7ea740c9e
Ian larson <94594696+CursedFaey@users.noreply.github.com>
2022-06-06 15:59:02 -0700

    project comments

--- src/main.js
+++ src/main.js
@@ -14,3 +14,9 @@ let borderPadding = borderUISize / 3;
 // reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT, keyA, keyD, keyJ, keyL, keyW, keyI;
 
+// Project Comments
+//  Point Breakdown:
+//  + (60) Redesign the Game's artwork, UI, and sound to change it's theme/aesthetic
+//  + (30) implement a simutaneous two-player mode
+//  + (10) Create 4 new explosion SFX and randomize which one plays on impact
+// total: (100)
