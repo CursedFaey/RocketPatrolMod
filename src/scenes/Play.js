0672e4e4c6407256b9419f7fc9d9154514c99914
Ian larson <94594696+CursedFaey@users.noreply.github.com>
2022-06-06 13:46:01 -0700

    fireworks explosions 4

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -16,24 +16,48 @@ class Play extends Phaser.Scene {
         this.load.image('p2Firework', './assets/p2Firework.png');
         this.load.image('p1Launcher', './assets/p1Launcher.png');
         this.load.image('p2Launcher', './assets/p2Launcher.png');
+        this.load.image('kite1', './assets/kite_01.png');
+        this.load.image('kite2', './assets/kite_02.png');
+        this.load.image('kite3', './assets/kite_03.png');
+        this.load.image('crowd1', './assets/crowd_01.png');
+        this.load.image('crowd2', './assets/crowd_02.png');
+        this.load.image('crowd3', './assets/crowd_03.png');
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
         this.load.spritesheet('p1Light', './assets/p1Launcher_light_sheet.png', {frameWidth: 64, frameHeight: 64, startFrame: 0, endFrame: 8});
         this.load.spritesheet('p2Light', './assets/p2Launcher_light_sheet.png', {frameWidth: 64, frameHeight: 64, startFrame: 0, endFrame: 8});
+        this.load.spritesheet('angry3', './assets/crowd_angry_01_sheet.png', {frameWidth: 64, frameHeight: 64, startFrame: 0, endFrame: 4});
+        this.load.spritesheet('angry2', './assets/crowd_angry_02_sheet.png', {frameWidth: 64, frameHeight: 64, startFrame: 0, endFrame: 4});
+        this.load.spritesheet('angry1', './assets/crowd_angry_03_sheet.png', {frameWidth: 64, frameHeight: 64, startFrame: 0, endFrame: 4});
+        this.load.spritesheet('explo1', './assets/explosion_01_sheet.png', {frameWidth: 96, frameHeight: 96, startFrame: 0, endFrame: 8});
+        this.load.spritesheet('explo2', './assets/explosion_02_sheet.png', {frameWidth: 96, frameHeight: 96, startFrame: 0, endFrame: 8});
+        this.load.spritesheet('explo3', './assets/explosion_03_sheet.png', {frameWidth: 96, frameHeight: 96, startFrame: 0, endFrame: 8});
+        this.load.spritesheet('explo4', './assets/explosion_04_sheet.png', {frameWidth: 96, frameHeight: 96, startFrame: 0, endFrame: 8});
     }
 
     create() {
         // place tile sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
+        // add Spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'kite3', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'kite2', 0, 20).setOrigin(0,0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'kite1', 0, 10).setOrigin(0,0);
+
+        this.crowd01 = new Spaceship(this, game.config.width + borderUISize*6, game.config.height - 105, 'crowd3', 0, 0).setOrigin(0, 0);
+        this.crowd02 = new Spaceship(this, game.config.width + borderUISize*3, game.config.height - 95, 'crowd2', 0, 0).setOrigin(0,0);
+        this.crowd03 = new Spaceship(this, game.config.width, game.config.height - 85, 'crowd1', 0, 0).setOrigin(0,0);
+        
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
-        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
-        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
-        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xAAFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFAA).setOrigin(0 ,0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFAAFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
+        
+        
         // add player rockets and p1 launcher
         this.launcher1 = this.add.sprite(game.config.width/2, game.config.height - borderUISize - borderPadding - 25, 'p1Launcher').setOrigin(0.5, 0);
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'p1Firework').setOrigin(0.5, 0);
@@ -45,11 +69,6 @@ class Play extends Phaser.Scene {
         if(game.settings.multiplayer){
             this.launcher2 = this.add.sprite(game.config.width/2, game.config.height - borderUISize - borderPadding - 25, 'p2Launcher').setOrigin(0.5, 0);
         }
-
-        // add Spaceships (x3)
-        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
-        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
-        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
 
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -78,6 +97,41 @@ class Play extends Phaser.Scene {
             key: 'light2',
             frames: this.anims.generateFrameNumbers('p2Light', { start: 0, end: 8, first: 0}),
             frameRate: 15
+        });
+        this.anims.create({
+            key: 'angryC1',
+            frames: this.anims.generateFrameNumbers('angry1', { start: 0, end: 4, first: 0}),
+            frameRate: 15
+        });
+        this.anims.create({
+            key: 'angryC2',
+            frames: this.anims.generateFrameNumbers('angry2', { start: 0, end: 4, first: 0}),
+            frameRate: 15
+        });
+        this.anims.create({
+            key: 'angryC3',
+            frames: this.anims.generateFrameNumbers('angry3', { start: 0, end: 4, first: 0}),
+            frameRate: 15
+        });
+        this.anims.create({
+            key: 'ex1',
+            frames: this.anims.generateFrameNumbers('explo1', { start: 0, end: 8, first: 0}),
+            frameRate: 30
+        });
+        this.anims.create({
+            key: 'ex2',
+            frames: this.anims.generateFrameNumbers('explo2', { start: 0, end: 8, first: 0}),
+            frameRate: 30
+        });
+        this.anims.create({
+            key: 'ex3',
+            frames: this.anims.generateFrameNumbers('explo3', { start: 0, end: 8, first: 0}),
+            frameRate: 30
+        });
+        this.anims.create({
+            key: 'ex4',
+            frames: this.anims.generateFrameNumbers('explo4', { start: 0, end: 8, first: 0}),
+            frameRate: 30
         });
 
         // initialize score
@@ -180,44 +234,47 @@ class Play extends Phaser.Scene {
             this.ship01.update();               // update spaceship (x3)
             this.ship02.update();
             this.ship03.update();
+            this.crowd01.update();              // update crowds (x3)
+            this.crowd02.update();
+            this.crowd03.update();
         }
 
         // check collisions player 1
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
-            this.shipExplode(this.ship03);
-            this.p1Rocket.reset();
+            this.shipExplode(this.ship03, this.p1Rocket);
+            this.anger3(this.crowd03);
             this.p1Score += this.ship03.points;
             this.scoreLeft.text = this.p1Score;
         }
         if (this.checkCollision(this.p1Rocket, this.ship02)) {
-            this.shipExplode(this.ship02);
-            this.p1Rocket.reset();
+            this.shipExplode(this.ship02, this.p1Rocket);
+            this.anger2(this.crowd02);
             this.p1Score += this.ship02.points;
             this.scoreLeft.text = this.p1Score;
         }
         if (this.checkCollision(this.p1Rocket, this.ship01)) {
-            this.shipExplode(this.ship01);
-            this.p1Rocket.reset();
+            this.shipExplode(this.ship01, this.p1Rocket);
+            this.anger1(this.crowd01);
             this.p1Score += this.ship01.points;
             this.scoreLeft.text = this.p1Score;
         }
 
         //check collisions player 2
         if(this.checkCollision(this.p2Rocket, this.ship03)) {
-            this.shipExplode(this.ship03);
-            this.p2Rocket.reset();
+            this.shipExplode(this.ship03, this.p2Rocket);
+            this.anger3(this.crowd03);
             this.p2Score += this.ship03.points;
             this.scoreRight.text = this.p2Score;
         }
         if (this.checkCollision(this.p2Rocket, this.ship02)) {
-            this.shipExplode(this.ship02);
-            this.p2Rocket.reset();
+            this.shipExplode(this.ship02, this.p2Rocket);
+            this.anger2(this.crowd02);
             this.p2Score += this.ship02.points;
             this.scoreRight.text = this.p2Score;
         }
         if (this.checkCollision(this.p2Rocket, this.ship01)) {
-            this.shipExplode(this.ship01);
-            this.p2Rocket.reset();
+            this.shipExplode(this.ship01, this.p2Rocket);
+            this.anger1(this.crowd01);
             this.p2Score += this.ship01.points;
             this.scoreRight.text = this.p2Score;
         }
@@ -235,29 +292,91 @@ class Play extends Phaser.Scene {
         }
     }
 
-    shipExplode(ship) {
+    shipExplode(ship, rocket) {
         // temporarily hide ship
         ship.alpha = 0;                         
         // create explosion sprite at ship's position
-        let burn = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
-        ship.reset();                           // reset ship position
-        burn.anims.play('explode');             // play explode animation
-        burn.on('animationcomplete', () => {    // callback after anim completes
-            ship.alpha = 1;                       // make ship visible again
-            burn.destroy();                       // remove explosion sprite
-        });
+        ship.reset();
         // randomly create a firework explosion
-        this.explo = Math.floor(Math.random()*4);
-        if(this.explo = 0){
+        let explo = Math.floor(Math.random()*4);
+        //console.log(explo);
+        if(explo <= 0){
+            let boom = this.add.sprite(rocket.x, rocket.y, 'explo1').setOrigin(0.5, 0.5);
+            rocket.reset();                           // reset ship position
+            boom.anims.play('ex1');             // play explode animation
+            boom.on('animationcomplete', () => {    // callback after anim completes
+                ship.alpha = 1;
+                boom.destroy();                       // remove explosion sprite
+            });
             this.sound.play('sfx_explosion');
-        }else if(this.explo = 1){
+        }else if(explo <= 1){
+            let boom = this.add.sprite(rocket.x, rocket.y, 'explo2').setOrigin(0.5, 0.5);
+            rocket.reset();                           // reset ship position
+            boom.anims.play('ex2');             // play explode animation
+            boom.on('animationcomplete', () => {    // callback after anim completes
+                ship.alpha = 1;
+                boom.destroy();                       // remove explosion sprite
+            });
             this.sound.play('sfx_explosion');
-        }else if(this.explo = 2){
+        }else if(explo <= 2){
+            let boom = this.add.sprite(rocket.x, rocket.y, 'explo3').setOrigin(0.5, 0.5);
+            rocket.reset();                           // reset ship position
+            boom.anims.play('ex3');             // play explode animation
+            boom.on('animationcomplete', () => {    // callback after anim completes
+                ship.alpha = 1;
+                boom.destroy();                       // remove explosion sprite
+            });
             this.sound.play('sfx_explosion');
         }else{
+            let boom = this.add.sprite(rocket.x, rocket.y, 'explo4').setOrigin(0.5, 0.5);
+            rocket.reset();                           // reset ship position
+            boom.anims.play('ex4');             // play explode animation
+            boom.on('animationcomplete', () => {    // callback after anim completes
+                ship.alpha = 1;
+                boom.destroy();                       // remove explosion sprite
+            });
             this.sound.play('sfx_explosion');
         }
       }
+
+    anger1(crowd) {
+        // temporarily hide ship
+        crowd.alpha = 0;                         
+        // create explosion sprite at ship's position
+        let angry = this.add.sprite(crowd.x, crowd.y, 'angry1').setOrigin(0, 0.5);
+        crowd.reset();                           // reset ship position
+        angry.anims.play('angryC1');             // play explode animation
+        angry.on('animationcomplete', () => {    // callback after anim completes
+            crowd.alpha = 1;                       // make ship visible again
+            angry.destroy();                       // remove explosion sprite
+        });
+    }
+    
+    anger2(crowd) {
+        // temporarily hide ship
+        crowd.alpha = 0;                         
+        // create explosion sprite at ship's position
+        let angry = this.add.sprite(crowd.x, crowd.y, 'angry2').setOrigin(0, 0.5);
+        crowd.reset();                           // reset ship position
+        angry.anims.play('angryC2');             // play explode animation
+        angry.on('animationcomplete', () => {    // callback after anim completes
+            crowd.alpha = 1;                       // make ship visible again
+            angry.destroy();                       // remove explosion sprite
+        });
+    }
+
+    anger3(crowd) {
+        // temporarily hide ship
+        crowd.alpha = 0;                         
+        // create explosion sprite at ship's position
+        let angry = this.add.sprite(crowd.x, crowd.y, 'angry3').setOrigin(0, 0.5);
+        crowd.reset();                           // reset ship position
+        angry.anims.play('angryC3');             // play explode animation
+        angry.on('animationcomplete', () => {    // callback after anim completes
+            crowd.alpha = 1;                       // make ship visible again
+            angry.destroy();                       // remove explosion sprite
+        });
+    }
 
       p1LaunchFirework() {
         // temporarily hide rocket   
